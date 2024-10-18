714ca3dc10850100100efaea5b27aa73b4ccbdbf
Daniel Aguayo <1136475+dantoac@users.noreply.github.com>
2024-10-17 21:06:10 -0300

    Enhance UI and functionality in HTML and Tailwind config
    Updated Tailwind configuration to include custom color palette. Improved header and section styles on the front page, integrated Poppins font, and added a contact form. Revised layout for better responsiveness and visual appeal.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["*.html"],
+    theme: {
+        extend: {
+            colors: {
+                'primary-blue': '#0056b3',
+                'secondary-blue': '#6c757d',
+                'light-blue': '#e6f2ff',
+                'dark-blue': '#003366',
+                'primary-green': '#27AE60',
+                'light-green': '#2ECC71',
+                'lime-green': '#c7eEa0',
+                'secondary-green': '#1ABC9C',
+                'dark-green': '#34495E',
+                primary: 'primary-blue'
+            }
+        }
+    },
 }
+
 
