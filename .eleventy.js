397c7226f54ce1d73aae22ac46934dbbe5d92df9
Daniel Aguayo <1136475+dantoac@users.noreply.github.com>
2024-10-30 15:33:20 -0300

    Add Eleventy configuration, base HTML template, and assets
    This commit initializes the Eleventy configuration with passthrough copy options for CSS and image files. It includes a comprehensive base HTML template and essential assets for the website. Finally, it updates the `.gitignore` to exclude the `public` directory.

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css/*.css", "/public/css/*.css")
+        .addPassthroughCopy("./src/images/*.{jpg,png,gif}")
+        .addPassthroughCopy("./src/posts/**/*.{jpg,png,gif}")
+        .addPassthroughCopy("./src/posts/blog/**/*.{jpg,png,gif}")
+    ;
+
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        }
+    }
+
+};
