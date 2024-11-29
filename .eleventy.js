08368bc8fc9f9fa6f9f71abd624dc998bdf09769
Daniel Aguayo <1136475+dantoac@users.noreply.github.com>
2024-11-29 12:33:45 -0300

    Add metadata and sitemap generation
    Introduced a metadata.json file to store site-specific details and added a new sitemap.njk template for generating the sitemap. Updated styling in base.html and styles.css to enhance presentation and introduced new filters in .eleventy.js to handle date formatting and URL generation.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/*.css", "/public/css/*.css")
         .addPassthroughCopy("./src/images/*.{jpg,png,gif}")
@@ -5,6 +7,16 @@ module.exports = function (eleventyConfig) {
         .addPassthroughCopy("./src/posts/blog/**/*.{jpg,png,gif}")
         .addPassthroughCopy("./src/_redirects")
     ;
+
+    // Filtro para convertir fechas a formato ISO
+    eleventyConfig.addFilter("dateToISO", (date) => {
+        return DateTime.fromJSDate(date).toISO();
+    });
+
+    // Filtro para URLs absolutas
+    eleventyConfig.addFilter("absoluteUrl", (url, base) => {
+        return new URL(url, base).toString();
+    });
 
     return {
         dir: {
